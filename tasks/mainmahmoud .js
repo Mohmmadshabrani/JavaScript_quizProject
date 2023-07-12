a347c006b7a028c9033256126d2fea0f71edccb1
mahmooud-jabali <mahmoudjabali56.xyx@gmail.com>
2023-07-12 16:46:13 +0300

    finish

---
+++ tasks/mainmahmoud .js
@@ -0,0 +1,118 @@
+
+
+
+//-----------------------------------
+
+//...task 1...//
+
+
+function tellFortune( job , location ,number,name){
+
+console.log("You will be a " + job + " in " +location +" and married to " +number+ " with " +name+ " kids")
+
+
+}
+tellFortune('software engineer' , `jordan` , 3 ,`areej`);
+
+
+
+
+//-------------------------------------------
+
+//...task 2...//
+
+
+function calculateDogAge( x){
+
+console.log("Your doggie is" +x*7+ "years old in dog years!");
+
+
+}
+calculateDogAge(2);
+
+
+
+
+
+
+
+//-----------------------------
+
+//...task 3...//
+
+
+function x (name){
+
+console.log(`hello`+ name);
+}
+
+x(`jabali`);
+x(`mohammad`)
+
+
+
+
+
+
+//-----------------------------
+
+//...task 4...//
+
+
+
+function  calculateSupply(age, amount_Per_Day){
+
+const max_Age=100
+x = max_Age -age 
+y = x*365*amount_Per_Day
+console.log("You will need" +y+ "to last you until the ripe old age of"+ max_Age)
+}
+calculateSupply(30, 3)
+
+
+
+
+
+//-----------------------------
+
+//...task 5...//
+
+//  what is the error:
+
+
+function double(cat) {
+  return 2 * cat;
+}
+
+function double(x) {
+  return 2 * x;
+}
+
+function double(x) {
+  return 2 * 'x';
+}
+
+
+
+
+
+
+//----------------------------
+
+//...task 6...//
+
+//  fix these functions:
+
+
+functions double1(x) {
+  return 2 * x ;
+}
+
+function double2 (x){
+return 2 * x;
+}
+
+function double3 (x){
+  return 2 * x;
+}
+
